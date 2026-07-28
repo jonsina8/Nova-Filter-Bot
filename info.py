b6f5ef9cd368dd383b0df8854d6b53ce55b80dab
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-07-28 18:06:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -142,17 +142,17 @@ PM_SEARCH = is_enabled('PM_SEARCH', True)
 
 # bot string settings
 FORCE_SUB_CHANNELS = environ.get('FORCE_SUB_CHANNELS', 'https://t.me/movies_updet')  # For multiple channels, separate channel IDs with spaces. Example: "-100xxxxx -100xxxxx -100xxxxx"
-REQUEST_FORCE_SUB_CHANNEL = environ.get('REQUEST_FORCE_SUB_CHANNEL', '')
+REQUEST_FORCE_SUB_CHANNEL = environ.get('REQUEST_FORCE_SUB_CHANNEL', 'https://t.me/movies_updet')
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002552925944")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", -1002552925944")
 if len(BIN_CHANNEL) == 0:
     logger.error('BIN_CHANNEL is missing, exiting now')
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "-1002552925944")
+URL = environ.get("URL", -1002552925944")
 if len(URL) == 0:
     logger.error('URL is missing, exiting now')
     exit()
